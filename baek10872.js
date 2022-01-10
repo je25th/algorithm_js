9837393b0232a7fe3c6e7de521843c65e0dde034
oij511 <oij511@naver.com>
2022-01-10 23:04:42 +0900

    백준 10872 팩토리얼 - node.js로 풀기 테스트용

---
+++ baek10872.js
@@ -0,0 +1,15 @@
+//팩토리얼 
+
+let fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().trim();
+
+let num = Number(input);
+console.log(solution(num));
+
+function solution(n) {
+  if(n == 1 || n == 0) {
+    return 1;
+  } else {
+    return n*solution(n-1);
+  }
+}
