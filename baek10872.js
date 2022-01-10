7eb71e41fdd57676e391d803d8e72676fea52c33
oij511 <oij511@naver.com>
2022-01-10 23:22:09 +0900

    잔디가 안생겨서 다시 커밋

--- baek10872.js
+++ baek10872.js
@@ -7,9 +7,6 @@ let num = Number(input);
 console.log(solution(num));
 
 function solution(n) {
-  if(n == 1 || n == 0) {
-    return 1;
-  } else {
-    return n*solution(n-1);
-  }
+  if(n == 1 || n == 0) return 1;
+  else return n*solution(n-1);
 }
